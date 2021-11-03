f553c7efcaf829f7a6b0f48907f15cf9c8591f03
Sachin <sachinkshuklaoo7@gmail.com>
2021-11-03 08:35:57 +0530

    added tests

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -1,5 +1,13 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+import "./Token.sol";
+
 contract EthSwap {
     string public name = "EthSwap Instant Exchange";
+    Token public token;
+    uint256 public rate = 100;
+
+    constructor(Token _token) public {
+        token = _token;
+    }
 }
---
+++ test/EthSwap.test.js
@@ -0,0 +1,42 @@
+const { default: Web3 } = require("web3");
+const { assert } = require("chai");
+
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+require("chai")
+    .use(require("chai-as-promised"))
+    .should();
+
+const tokens = (n) => {
+    return web3.utils.toWei(n, "ether");
+};
+
+contract("EthSwap", ([deployer, investor]) => {
+    let token, ethSwap;
+
+    before(async () => {
+        token = await Token.new();
+        ethSwap = await EthSwap.new(token.address);
+        // Transfer all the tokens to EthSwap (1 M)
+        await token.transfer(ethSwap.address, tokens("1000000"));
+    });
+
+    describe("Token Deployement", async () => {
+        it("Contract has a name", async () => {
+            const name = await token.name();
+            assert.equal(name, "DApp Token");
+        });
+    });
+
+    describe("EthSwap Deployement", async () => {
+        it("Contract has a name", async () => {
+            const name = await ethSwap.name();
+            assert.equal(name, "EthSwap Instant Exchange");
+        });
+
+        it("Contract has tokens", async () => {
+            const balance = await token.balanceOf(ethSwap.address);
+            assert.equal(balance.toString(), tokens("1000000"));
+        });
+    });
+})
