3edcd6a16112afde8aeb3559dc932c793150ea26
Sachin <sachinkshuklaoo7@gmail.com>
2021-11-10 22:53:10 +0530

    added sell tokens

--- src/contracts/EthSwap.sol
+++ src/contracts/EthSwap.sol
@@ -18,6 +18,13 @@ contract EthSwap {
         uint256 rate
     );
 
+    event TokensSold(
+        address account,
+        address token,
+        uint256 amount,
+        uint256 rate
+    );
+
     function buyTokens() public payable {
         uint256 tokenAmount = msg.value * rate;
         token.transfer(msg.sender, tokenAmount);
@@ -25,5 +32,23 @@ contract EthSwap {
         require(token.balanceOf(address(this)) >= tokenAmount);
 
         emit TokensPurchased(msg.sender, address(token), tokenAmount, rate);
+    }
+
+    function sellTokens(uint256 _amount) public {
+        // user cant sell more tokens than they have
+        require(token.balanceOf(msg.sender) >= _amount);
+
+        // Calculate te amount of Ether to redeem
+        uint256 etherAmount = _amount * rate;
+
+        // Require ethSwap has enough Ether
+        require(address(this).balance >= etherAmount);
+
+        // Perform sale
+        token.transferFrom(msg.sender, address(this), _amount);
+        msg.sender.transfer(etherAmount);
+
+        // emit an event
+        emit TokensSold(msg.sender, address(token), _amount, rate);
     }
 }
--- test/EthSwap.test.js
+++ test/EthSwap.test.js
@@ -40,7 +40,7 @@ contract("EthSwap", ([deployer, investor]) => {
         });
     });
 
-    describe('Buy Tokens', async () => {
+    describe("buyTokens()", async () => {
         let result;
 
         before(async () => {
@@ -48,10 +48,10 @@ contract("EthSwap", ([deployer, investor]) => {
             result = await ethSwap.buyTokens({ from: investor, value: tokens("1") });
         });
 
-        it("Allows user to instantly purchase tokens from EthSwap for a fixed price", async () => {
-            // Check investor balance
-            let investorBalance = await token.balanceOf(investor);
-            assert.equal(investorBalance.toString(), tokens('100'));
+        it("Allows users to instatly buy token from ethSwap for a fixed price", async () => {
+            // Check Investor balance
+            let inverstorBalance = await token.balanceOf(investor);
+            assert.equal(inverstorBalance.toString(), tokens("100"));
 
             // Check ethSwap balance
             let ethSwapBalance;
@@ -66,6 +66,37 @@ contract("EthSwap", ([deployer, investor]) => {
             assert.equal(event.token, token.address);
             assert.equal(event.amount.toString(), tokens("100"));
             assert.equal(event.rate.toString(), "100");
-        })
-    })
-})
+        });
+    });
+
+    describe("sellTokens()", async () => {
+        let result;
+
+        before(async () => {
+            // Investor must approve the purchase
+            await token.approve(ethSwap.address, tokens("1"), { from: investor });
+            result = await ethSwap.sellTokens(tokens("1"), { from: investor });
+        });
+        it("Allows users to instantly sell tokens to ethSwap for a fixed price", async () => {
+            let inverstorBalance = await token.balanceOf(investor);
+            assert.equal(inverstorBalance.toString(), tokens("0"));
+
+            // Check ethSwap balance
+            let ethSwapBalance;
+            ethSwapBalance = await token.balanceOf(ethSwap.address);
+            assert.equal(ethSwapBalance.toString(), tokens("1000000"));
+            ethSwapBalance = await web3.eth.getBalance(ethSwap.address);
+            assert.equal(ethSwapBalance.toString(), tokens("0"));
+
+            // Check the event details
+            const event = result.logs[0].args;
+            assert.equal(event.account, investor);
+            assert.equal(event.token, token.address);
+            assert.equal(event.amount.toString(), tokens("1"));
+            assert.equal(event.rate.toString(), "100");
+
+            // Failure: inverstors cannot sell more tokens than they have
+            await ethSwap.sellTokens(tokens("500"), { from: investor }).should.be.rejected;
+        });
+    });
+});
